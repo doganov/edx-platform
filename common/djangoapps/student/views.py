235b4cb5ba3606bb3bd2272f24054bd82a492222
Jonathan Piacenti <kelketek@gmail.com>
2015-11-04 22:19:17 +0000

    More notes.

--- common/djangoapps/student/views.py
+++ common/djangoapps/student/views.py
@@ -460,7 +460,7 @@ def register_user(request, extra_context=None):
             overrides['selected_provider'] = current_provider.name
             context.update(overrides)
 
-    context.update(registration_dropdown_context())
+    context.update(registration_dropdown_context()._asdict())
 
     return render_to_response('register.html', context)
 
